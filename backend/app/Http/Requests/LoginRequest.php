476b4951c360e8d407a1912fd881b1239b34d1c8
YoussefFakhi <youssef.fakhi.dev@gmail.com>
2026-02-05 13:35:27 +0100

    feat: Add login validation request

---
+++ backend/app/Http/Requests/LoginRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;   //🐦 change it from false to true  this allows requests to be autorize
+                       // so validation rules can run
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+            return [
+                'email'=>'required|email',
+                'password'=>'required|string'
+            ];
+    }
+}
