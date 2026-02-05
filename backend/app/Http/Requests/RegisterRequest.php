47b5d7478f50bf1a8b18bd218a05d6da73c45420
YoussefFakhi <youssef.fakhi.dev@gmail.com>
2026-02-05 13:35:04 +0100

    feat: Add registration validation request

---
+++ backend/app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true; //🐦 change it from false to true  this allows requests to be autorize
+                       // so validation rules can run
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'=>'required|string|max:255',
+            'email'=>'required|email|unique:users,email',
+            'password'=>'required|confirmed|max:8',
+            'role' => 'nullable|in:client,freelancer'
+        ];
+    }
+}
