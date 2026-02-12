9794c71ba9022f2b35a5ecb19657e29a35bdb212
YoussefFakhi <youssef.fakhi.dev@gmail.com>
2026-02-12 15:23:39 +0100

    feat: create fake Users

---
+++ backend/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin'
+        ]);
+
+        User::create([
+            'name' => 'Client User',
+            'email' => 'client@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'client'
+        ]);
+
+        User::create([
+            'name' => 'Freelancer User',
+            'email' => 'freelancer@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'freelancer'
+        ]);
+    }
+}
